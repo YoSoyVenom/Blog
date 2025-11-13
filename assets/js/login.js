404043d6d6604c3127ecec8d4a60cafbd13aa596
Sebastian Guevara <joanmarin2021@gmail.com>
2025-11-13 14:22:00 -0500

    Conectando todas las partes de la página con JavaScript.

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,12 @@
 const menuToggle = document.querySelector('.encabezado__desplegar');
 const menuList = document.querySelector('.encabezado__navegacion');
+const btnLogin = document.getElementById('btn-login');
 
 menuToggle.addEventListener('click', () => {
     menuList.classList.toggle('encabezado__lista--visible');
+});
+
+btnLogin.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'main.html';
 });
---
+++ assets/js/logup.js
@@ -0,0 +1,7 @@
+const btnRegister = document.getElementById('btn-register');
+
+btnRegister.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'main.html';
+});
+
