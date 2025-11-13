f86d1c516ba542e17cb8b6667e8dbf15a77a4d33
Sebastian Guevara <joanmarin2021@gmail.com>
2025-11-13 14:49:20 -0500

    Empezando correcciones con el aside en mobile.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
-const aside = document.querySelector(".sidebar");
-const btnProfile = document.querySelector(".encabezado__user");
+const aside = document.querySelector("#sidebar");
+const btnProfile = document.querySelector("#btn-profile");
+const header = document.querySelector("#encabezado-position");
 btnProfile.addEventListener("click", () => {
-    aside.classList.toggle("visible");
+    aside.classList.toggle("aside-visible");
+    header.classList.toggle("header-zindex");
 })
 
