ea1acb4bbcf546f33d444b32a98a4d60c6b611d8
Sebastian Guevara <joanmarin2021@gmail.com>
2025-11-14 13:53:02 -0500

    Dar nuevos estilos en mobile-first para el aside.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
 const aside = document.querySelector("#sidebar");
 const btnProfile = document.querySelector("#btn-profile");
-const header = document.querySelector("#encabezado-position");
+
 btnProfile.addEventListener("click", () => {
     aside.classList.toggle("aside-visible");
-    header.classList.toggle("header-zindex");
 })
 
