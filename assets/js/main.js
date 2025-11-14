98d2d3b422e23caba4ff082f6b8f3ebe62290787
Sebastian Guevara <joanmarin2021@gmail.com>
2025-11-14 14:24:43 -0500

    Estilos aside en mobile listos.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 const aside = document.querySelector("#sidebar");
 const btnProfile = document.querySelector("#btn-profile");
+const header = document.querySelector("#encabezado");
+
+const headerHeight = header.offsetHeight;
+aside.style.top = `${headerHeight}px`;
+aside.style.height = `calc(100dvh - ${headerHeight}px)`;
 
 btnProfile.addEventListener("click", () => {
     aside.classList.toggle("aside-visible");
