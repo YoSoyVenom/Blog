52fb28805c86954107fc6622bb383b100ef70061
Sebastian Guevara <joanmarin2021@gmail.com>
2025-11-15 14:05:02 -0500

    Estilos de aside en desktop completos.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,20 @@ const btnProfile = document.querySelector("#btn-profile");
 const header = document.querySelector("#encabezado");
 
 const headerHeight = header.offsetHeight;
-aside.style.top = `${headerHeight}px`;
-aside.style.height = `calc(100dvh - ${headerHeight}px)`;
 
-btnProfile.addEventListener("click", () => {
-    aside.classList.toggle("aside-visible");
-})
-
+function ajustarClasePorResolucion() {
+    const anchoVentana = window.innerWidth;
+    console.log(anchoVentana);
+    if (anchoVentana < 768) {
+        aside.style.top = `${headerHeight}px`;
+        aside.style.height = `calc(100dvh - ${headerHeight}px)`;
+        btnProfile.addEventListener("click", () => {
+            aside.classList.toggle("aside-visible");
+        })
+    } else {
+        aside.style.top = `calc(${headerHeight}px + 20px)`;
+        aside.classList.remove("aside-visible");
+    }
+}
+window.addEventListener("resize", ajustarClasePorResolucion);
+ajustarClasePorResolucion();
