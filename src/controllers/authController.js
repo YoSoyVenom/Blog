59f4c04d0501f895570e49788a4d8f21fd8661ca
Sebastian Guevara <joanmarin2021@gmail.com>
2025-12-01 14:32:29 -0500

    Inicio de sesion con contraseñas encriptadas.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/userModels");
 const crypto = require("crypto");
+const bcrypt = require("bcrypt");
 
 // INICIO DE SESION.
 exports.login = (req, res) => {
@@ -9,16 +10,12 @@ exports.login = (req, res) => {
     if (!user) {
         res.status(404).json({message: "Email de usuario no encontrado..."});
     }
-    if (user.password_hash !== password) {
+    const hashedPassword = bcrypt.compareSync(password, user.password);
+    if (!hashedPassword) {
         res.status(401).json({message: "Contraseña incorrecta"});
     }
 
     res.json({
         message: "Inicio de sesión exitoso",
-        user: {
-            id: user.id,
-            email: user.email,
-            password: user.password
-        }
     });
 };
