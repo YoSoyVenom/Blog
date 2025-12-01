257f789b17a0f57a3a0594e0ff29936cb19824ff
Sebastian Guevara <joanmarin2021@gmail.com>
2025-12-01 15:44:28 -0500

    Registro incompleto con errores lógicos.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,30 @@ const userModel = require("../models/userModels");
 const crypto = require("crypto");
 const bcrypt = require("bcrypt");
 
+// REGISTRO.
+
+exports.register = (req, res) => {
+    const { username, email, password } = req.body;
+    const user = userModel.findUserByEmail(email);
+    if (user) {
+        res.status(400).json({ message: "Este email ya está registrado." });
+    }
+    if (user.username === username) {
+        res.status(400).json({ message: "Este nombre de usuario ya existe" })
+    }
+    const newUser = {
+        id: crypto.randomUUID(),
+        username: username,
+        email: email,
+        password_hash: bcrypt.hashSync(password),
+        profile_picture: null,
+        bio: "",
+        created_at: ""
+    }
+    userModel.createUser(newUser);
+    res.json({ message: "Usuario creado con exito." });
+}
+
 // INICIO DE SESION.
 exports.login = (req, res) => {
     const { email, password } = req.body;
@@ -10,8 +34,8 @@ exports.login = (req, res) => {
     if (!user) {
         res.status(404).json({message: "Email de usuario no encontrado..."});
     }
-    const hashedPassword = bcrypt.compareSync(password, user.password);
-    if (!hashedPassword) {
+    const passwordMatches = bcrypt.compareSync(password, user.password);
+    if (!passwordMatches) {
         res.status(401).json({message: "Contraseña incorrecta"});
     }
 
