b73b657ad503c7b6c3c3af8ecf079200c6d93b69
Sebastian Guevara <joanmarin2021@gmail.com>
2025-12-02 14:11:05 -0500

    LogIn funcional sin JWT.

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,40 @@
 const btnLogin = document.getElementById('btn-login');
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 
-btnLogin.addEventListener('click', function(event) {
-    event.preventDefault();
-    window.location.href = 'main.html';
-});
+btnLogin.addEventListener('click', iniciarSesion);
+
+async function iniciarSesion(e) {
+    e.preventDefault();
+    const URL = "http://localhost:3200/api/auth/login";
+    const credenciales = {
+        email: email.value,
+        password: password.value
+    }
+    try {
+        // PETICIÓN PARA COMPROBAR LOS DATOS DEL USUARIO.
+        const response = await fetch(URL, {
+            method: "POST",
+
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify(credenciales)
+        });
+
+        const data = await response.json();
+        const message = data.message;
+
+        // SI LA PETICIÓN SE CUMPLE.
+        if (response.ok) {
+            window.location = "/";
+            console.log(message);
+        } else {
+            console.log(`Fallo en la autentificación ${response.status}: ${message}`)
+            alert(message);
+        }
+    } catch (error) {
+        console.log(`Error de red: ${error.message}`);
+    }
+}
