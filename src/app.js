fbb10973be44cbb8c639a28c797a9509d97752a2
Sebastian Guevara <joanmarin2021@gmail.com>
2025-12-02 11:13:45 -0500

    Limite de intentos en el Inicio de Sesión.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const rateLimit = require("express-rate-limit");
 
 // PARA MANEJAR REQ.BODY
 app.use(express.json());
@@ -8,6 +9,13 @@ app.use(express.json());
 // CARGA ARCHIVOS CSS, JS, IMÁGENES, ETC.
 const RUTA_ESTATICA = path.join(__dirname, "..", "public");
 app.use(express.static(RUTA_ESTATICA));
+
+// FUNCIÓN PARA EL LOGIN.
+const loginLimiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 5,
+    message: {message: "Demasiados intentos. Intenta en un minuto"}
+});
 
 // RUTAS.
 // CARGAR ARCHIVOS HTML.
@@ -17,5 +25,7 @@ app.use("/", viewRouter);
 // CARGAR LOGICA DE INICIO DE SESIÓN Y REGISTRO.
 const authRoutes = require("./routes/authRoutes.js");
 app.use("/api/auth", authRoutes);
+
+app.use("/api/auth/login", loginLimiter);
 
 module.exports = app;
