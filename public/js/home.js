2bec11170305d58e2e7411ab9e5b945a1f10e403
Sebastian Guevara <joanmarin2021@gmail.com>
2025-12-04 10:51:39 -0500

    Funcionalidades de la página principal.

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,6 @@
-const aside = document.querySelector("#sidebar");
-const btnProfile = document.querySelector("#btn-profile");
-const header = document.querySelector("#encabezado");
+const aside = document.getElementById("sidebar");
+const btnAsideProfile = document.getElementById("btn-profile");
+const header = document.getElementById("encabezado");
 
 const headerHeight = header.offsetHeight;
 
@@ -9,13 +9,16 @@ function ajustarClasePorResolucion() {
     if (anchoVentana < 768) {
         aside.style.top = `${headerHeight}px`;
         aside.style.height = `calc(100dvh - ${headerHeight}px)`;
-        btnProfile.addEventListener("click", () => {
+        btnAsideProfile.style.display = "block";
+        btnAsideProfile.addEventListener("click", () => {
             aside.classList.toggle("aside-visible");
         })
     } else {
         aside.style.top = `calc(${headerHeight}px + 20px)`;
         aside.classList.remove("aside-visible");
+        btnAsideProfile.style.display = "none";
     }
 }
 window.addEventListener("resize", ajustarClasePorResolucion);
 ajustarClasePorResolucion();
+
