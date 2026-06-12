862d47eecc596ca5c168bd58036ae9ded544198f
Sebastian Guevara <joanmarin2021@gmail.com>
2026-06-12 06:20:55 -0500

    Actualizando a db con PostgreSQL

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
 const app = require("./app");
 const PORT = 3200;
+const pool = require("./config/db_config");
+
+app.get("/db_prueba", async(req,res) =>{
+    try {
+        const results = await pool.query("SELECT * FROM users");
+        if (results.rows.length > 0) {
+            res.status(200).send(results);
+        } else {
+            res.status(200).json({ message: "Without information" })
+        }
+    } catch (error) {
+        res.json({ message: "Has been ocurred an error", error: error })
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en ${PORT}`);
