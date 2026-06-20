09cff261adf282c207e941a1c8a91f74f9d55448
Sebastian Guevara <joanmarin2021@gmail.com>
2026-06-20 11:45:38 -0500

    userService con las funciones importantes de LogIn y SignUp

---
+++ src/services/userService.js
@@ -0,0 +1,44 @@
+const pool = require("../config/db_config");
+
+//Funciones de Búsqueda.
+
+async function findUserByEmail (email) {
+    const QUERY = 'SELECT * FROM users WHERE email = $1 RETURNING password_hash';
+
+    const result = await pool.query(QUERY, [email]);
+
+    if (result.rows.length === 0) return null;
+
+    return result.rows[0].password_hash;
+}
+
+async function findUserById(id) {
+    const QUERY = 'SELECT * FROM users WHERE user_id = $1';
+
+    const result = await pool.query(QUERY, [id]);
+    
+    if (result.rows.length === 0) return null;
+
+    return result.rows[0];
+}
+
+// Funciones de Registro.
+
+async function createUser(userData) {
+    const QUERY = 'INSERT INTO users (username, email, password_hash, bio) VALUES ($1, $2, $3, $4) RETURNING user_id';
+    
+    const result = await pool.query(QUERY, [
+        userData.username, 
+        userData.email, 
+        userData.password_hash, 
+        userData.bio
+    ]);
+
+    return result.rows[0].user_id;
+}
+
+module.exports = {
+    findUserByEmail,
+    findUserById,
+    createUser
+}
